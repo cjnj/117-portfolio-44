c9874bab095e8a1ef77390a0b17524e04e4c5b81
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-05-14 22:57:56 -0400

    117 last class

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import projects_view
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
 
     path("", projects_view, name="projects"),
     
 ]
-   
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
